cbc9732a201e98fa6d3a23ff0a67065390167cbb
coder <usmccoder@gmail.com>
2024-03-22 12:12:27 -0700

    add services

---
+++ src/Services/CaptchaService.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace SgtCoder\LaravelFunctions\Services;
+
+use Illuminate\Support\Facades\{
+	Http,
+	Log
+};
+use Illuminate\Validation\{
+	Rule,
+	ValidationException
+};
+
+class CaptchaService
+{
+	public function verify_captcha()
+	{
+		request()->validate(
+			[
+				'g-recaptcha-response' => 'required',
+			],
+			[
+				'g-recaptcha-response.required' => 'Captcha Required',
+			]
+		);
+
+		$url = 'https://www.google.com/recaptcha/api/siteverify';
+		$fields = array(
+			'secret' => config('captcha.private_key'),
+			'response' => request('g-recaptcha-response'),
+			'remoteip' => request()->getClientIp(),
+		);
+
+		$response = Http::asForm()->post($url, $fields)->json();
+
+		if ($response['success']) {
+			return true;
+		}
+
+		Log::channel('stack')->error('Captcha V3 Failed', $response);
+
+		throw ValidationException::withMessages(['g-recaptcha-response' => 'Captcha Invalid']);
+	}
+
+	public function verify_captcha_v3()
+	{
+		// @phpstan-ignore-next-line
+		$response = \GoogleReCaptchaV3::verifyResponse(
+			request('g-recaptcha-response'),
+			request()->getClientIp()
+		);
+
+		if ($response->isSuccess()) {
+			return true;
+		}
+
+		Log::channel('stack')->error('Captcha V3 Failed: ' . $response->getMessage());
+
+		throw ValidationException::withMessages(['g-recaptcha-response' => 'Captcha Invalid']);
+	}
+
+	public function turnstile()
+	{
+		request()->validate([
+			// @phpstan-ignore-next-line
+			'cf-turnstile-response' => ['required', Rule::turnstile()],
+		]);
+	}
+
+	public function honeypot()
+	{
+		request()->validate([
+			'my_name'   => 'honeypot',
+			'my_time'   => 'required|honeytime:5'
+		]);
+	}
+}
---
+++ src/Services/PasswordService.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace SgtCoder\LaravelFunctions\Services;
+
+class PasswordService
+{
+    public function generate_password()
+    {
+        $type = request('type');
+        $length = request('length');
+
+        if (empty($type)) return response()->json(['status' => false, 'message' => 'You must pass a type'], 422);
+
+        return response()->json($this->$type($length), 200);
+    }
+
+    public function uuid($length = 36)
+    {
+        return str()->uuid();
+    }
+
+    public function hex($length = 16)
+    {
+        $token = hash('sha256', str()->random($length));
+        $token = str()->of($token)->substr(0, $length);
+        $token = str()->of($token)->upper();
+
+        return $token;
+    }
+
+    public function bearer($length = 40)
+    {
+        return str()->random($length);
+    }
+
+    public function redis($length = 16)
+    {
+        return str()->random($length);
+    }
+
+    public function string($length = 16)
+    {
+        return str()->random($length);
+    }
+
+    public function salt($length = 64)
+    {
+        return str()->password($length);
+    }
+
+    public function password($length = 32)
+    {
+        $is_valid = false;
+
+        while (!$is_valid) {
+            $password = str()->password($length);
+            $password = str()->replace('\\', '/', $password);
+
+            $is_valid = str()->of($password)->substr(0, 1)->isMatch('/[A-Za-z]/');
+        }
+
+        return $password;
+    }
+
+    public function mac($length = 17)
+    {
+        return implode('', generate_mac_address());
+    }
+
+    public function number($length = 8)
+    {
+        $number = null;
+        for ($i = 0; $i < $length; $i++) {
+            $number .= mt_rand(0, 9);
+        }
+
+        return $number;
+    }
+}
