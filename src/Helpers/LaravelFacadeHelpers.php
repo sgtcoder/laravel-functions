fb960b971fc506d5415d9b395689699885afb915
coder <usmccoder@gmail.com>
2025-12-16 07:46:11 -0700

    helper function updates

---
+++ src/Helpers/LaravelFacadeHelpers.php
@@ -0,0 +1,61 @@
+<?php
+
+if (!function_exists('http')) {
+    /**
+     * http
+     *
+     * @return \Illuminate\Support\Facades\Http
+     */
+    function http()
+    {
+        return \Illuminate\Support\Facades\Http::getFacadeRoot();
+    }
+}
+
+if (!function_exists('laravel_cookie')) {
+    /**
+     * laravel_cookie
+     *
+     * @return \Illuminate\Support\Facades\Cookie
+     */
+    function laravel_cookie()
+    {
+        return \Illuminate\Support\Facades\Cookie::getFacadeRoot();
+    }
+}
+
+if (!function_exists('artisan')) {
+    /**
+     * artisan
+     *
+     * @return \Illuminate\Support\Facades\Artisan
+     */
+    function artisan()
+    {
+        return \Illuminate\Support\Facades\Artisan::getFacadeRoot();
+    }
+}
+
+if (!function_exists('storage')) {
+    /**
+     * storage
+     *
+     * @return \Illuminate\Support\Facades\Storage
+     */
+    function storage()
+    {
+        return \Illuminate\Support\Facades\Storage::getFacadeRoot();
+    }
+}
+
+if (!function_exists('file')) {
+    /**
+     * file
+     *
+     * @return \Illuminate\Support\Facades\File
+     */
+    function file()
+    {
+        return \Illuminate\Support\Facades\File::getFacadeRoot();
+    }
+}
